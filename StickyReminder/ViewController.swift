387b5095eea4f72b77408d3ae4b48a2e5798984e
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2021-12-27 19:26:26 +0530

    Initial Commit

---
+++ StickyReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyReminder
+//
+//  Created by Prateek on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
